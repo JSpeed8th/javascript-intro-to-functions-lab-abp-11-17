309e3eafebe081ac9ec97244e9d32f72aa811338
JSpeed8th <jspeed8th@gmail.com>
2017-12-07 01:11:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,10 +12,13 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if (string.toLowerCase()) {
+  if (string.toLowerCase() === string) {
   return "I can't hear you!"
 }
-  if (string.toUpperCase()) {
+  else if (string.toUpperCase() === string) {
     return "YES INDEED!"
+  }
+  else if (string === "I love you, Grandma") {
+    return "I love you, too"
   }
 }
